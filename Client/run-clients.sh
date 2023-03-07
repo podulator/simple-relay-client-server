3d6643156415cf9ec2d41767d3332b2e19ea6d89
podulator <github-account@podulator.com>
2023-03-07 05:54:26 +0100

    updated individual run scripts

--- Client/run-clients.sh
+++ Client/run-clients.sh
@@ -3,5 +3,5 @@
 server="127.0.0.1"
 region=$(cat ~/.aws/config | grep region | cut -f2 -d'=' | xargs)
 
-./bin/Release/netcoreapp7.0/Client -s ${server} -p 8080 -n ga-client-${region} -f demo- -k ${region}:ga-demo-matrow-${region} &
-./bin/Release/netcoreapp7.0/Client -s ${server} -p 8080 -n no-ga-client-${region} -f demo- -k ${region}:ga-demo-matrow-${region} &
+./bin/Release/netcoreapp7.0/Client -s ${server} -p 8080 -n ga-client-${region} -f demo- -k ${region}:ga-demo-${region} &
+./bin/Release/netcoreapp7.0/Client -s ${server} -p 8080 -n no-ga-client-${region} -f demo- -k ${region}:ga-demo-${region} &
--- Server/run-server.sh
+++ Server/run-server.sh
@@ -2,4 +2,4 @@
 
 region=$(cat ~/.aws/config | grep region | cut -f2 -d'=' | xargs)
 
-./bin/Release/netcoreapp7.0/Server  -p 8080 -n server-${region} -f demo- -k ${region}:ga-demo-matrow-${region} -v &
+./bin/Release/netcoreapp7.0/Server  -p 8080 -n server-${region} -f demo- -k ${region}:ga-demo-${region} -v &
