cc5b8fbd33df9cd4337800aa64948d399812bdec
podulator <github-account@podulator.com>
2023-03-07 05:53:00 +0100

    updated client run container shellscript

--- Client/run-container.sh
+++ Client/run-container.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+server="127.0.0.1"
+region=$(cat ~/.aws/config | grep region | cut -f2 -d'=' | xargs)
 pid=$$
+
 docker run \
-	--name client-${pid} \
+    --name client-${pid} \
     --rm \
     --network="host"  \
-	-it podulator/performance-test-client:0.1 \
-    -s 127.0.0.1 -p 8080 -n client-%region%
+    -it podulator/performance-test-client:0.1 \
+    -s ${server} \
+    -p 8080 \
+    -n ga-client-${region} \
+    -f demo- \
+    -k ${region}:ga-demo-matrow-${region}
